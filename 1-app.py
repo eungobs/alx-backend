67c236fa1d0b00df44d13c489453b35062a07137
eungobs <eungobs@gmail.com>
2024-04-09 19:18:15 +0000

    1-app.py

--- 1-app.py
+++ 1-app.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python3
-"""A babel and flask app"""
+"""
+Basic Babel setup
+"""
 
-from flask import Flask, render_template
+import babel
+from flask import Flask, render_template, request
 from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
 
 class Config:
     """
-    Setting the languages and the locale and timezone
+    Config class
     """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-app = Flask(__name__)
+
 app.config.from_object(Config)
-babel = Babel(app)
 
-@app.route('/')
-def hello_world():
-    """to run on the website"""
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
+    """
     return render_template('1-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(debug=True)
